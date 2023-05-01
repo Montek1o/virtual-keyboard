c796c94d56a18007d6f377d2225c43056387043e
Andrey Nezhdanov <nezhd.an@yandex.ru>
2023-05-02 02:11:19 +0300

    feat: pressing a key on a physical keyboard highlights the key on the virtual keyboard

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const keys = ['ё', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace',
+const keys = ['`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace',
   'Tab', 'й', 'ц', 'у', 'к', 'е', 'н', 'г', 'ш', 'щ', 'з', 'х', 'ъ', '\\', 'Del',
   'Caps Lock', 'ф', 'ы', 'в', 'а', 'п', 'р', 'о', 'л', 'д', 'ж', 'э', 'Enter',
   'Shift', 'я', 'ч', 'с', 'м', 'и', 'т', 'ь', 'б', 'ю', '.', '▲', 'Shift',
@@ -60,3 +60,50 @@ function createLayout() {
 }
 
 createLayout();
+
+// pressing a key
+
+document.addEventListener('keydown', (event) => {
+  const key = document.querySelectorAll('.key');
+  console.log(event.key);
+  key.forEach((elem) => {
+    if (event.key == elem.innerHTML) {
+      elem.classList.add('keydown');
+    }
+    if (event.key == elem.innerHTML.toUpperCase()) {
+      elem.classList.add('keydown');
+    }
+    if (event.key == 'CapsLock' && elem.innerHTML == 'Caps Lock') {
+      elem.classList.add('keydown');
+    }
+    if (event.key == 'Control' && elem.innerHTML == 'Ctrl') {
+      elem.classList.add('keydown');
+    }
+    if (event.key == 'Meta' && elem.innerHTML == 'Win') {
+      elem.classList.add('keydown');
+    }
+    if (event.key == 'ArrowUp' && elem.innerHTML == '▲') {
+      elem.classList.add('keydown');
+    }
+    if (event.key == 'ArrowDown' && elem.innerHTML == '▼') {
+      elem.classList.add('keydown');
+    }
+    if (event.key == 'ArrowLeft' && elem.innerHTML == '◄') {
+      elem.classList.add('keydown');
+    }
+    if (event.key == 'ArrowRight' && elem.innerHTML == '►') {
+      elem.classList.add('keydown');
+    }
+    if (event.key == 'Delete' && elem.innerHTML == 'Del') {
+      elem.classList.add('keydown');
+    }
+  })
+});
+
+document.addEventListener('keyup', (event) => {
+  const key = document.querySelectorAll('.key');
+
+  key.forEach((elem) => {
+    elem.classList.remove('keydown');
+  })
+});
